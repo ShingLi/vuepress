622bd425c582bd53509fa11580dc7392c7815ea8
shingli <745573545@qq.com>
2018-08-30 17:56:59 +0800

    zzzz

--- docs/.vuepress/components/about.vue
+++ docs/.vuepress/components/about.vue
@@ -7,15 +7,18 @@
 					<el-aside width='20%'>
 						<div class="userinfo">
 				            <div class="avatar">
-				                <img :src="$withBase('/img/avatar.jpg')">
+				                <img :src="$withBase(`${avatar}?imageView2/1/w/80/h/80`)">
 				            </div>
 							<div class="base_wrapper">
 								<div class="base_info">
 									<p class="left"></p>
 									<p class="base">个人信息</p>
 								</div>
-                                <div class="item">
-                                    <i></i><span>男</span>
+                                <div class="item"
+                                    v-for='item in bossInfo'
+                                >
+                                    <i class="iconfont" v-html='item.icon'></i>
+                                    <span>{{item.text}}</span>
                                 </div>
                                 
 							</div>
@@ -32,8 +35,16 @@
 		name:'about',
 		data() {
 			return {
-				name:'李成',
-                bossInfo:[ '男', '汉族', '安徽芜湖', '745573545@qq.com']
+                name: '李成',
+                avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+                bossInfo: [
+                    {icon:'&#xe66e;',text:'男'},
+                    {icon:'&#xe626;',text:'汉'},
+                    {icon:'&#xe608;',text:'licheng'},
+                    {icon:'&#xe60d;',text:'安徽芜湖'},
+                    {icon:'&#xe670;',text:'745573545@qq.com'}
+
+                ]
 			}
 		},
         created() {
@@ -43,13 +54,27 @@
 </script>
 <style lang="less" scoped>
     @import '../public/styles/variable.less';
+    @font-face {
+        font-family: 'iconfont';  /* project id 439555 */
+        src: url('//at.alicdn.com/t/font_439555_nrqz7rbcpjr.eot');
+        src: url('//at.alicdn.com/t/font_439555_nrqz7rbcpjr.eot?#iefix') format('embedded-opentype'),
+        url('//at.alicdn.com/t/font_439555_nrqz7rbcpjr.woff') format('woff'),
+        url('//at.alicdn.com/t/font_439555_nrqz7rbcpjr.ttf') format('truetype'),
+        url('//at.alicdn.com/t/font_439555_nrqz7rbcpjr.svg#iconfont') format('svg');
+    }
+    .iconfont{
+        font-family:"iconfont" !important;
+        font-size:16px;font-style:normal;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-stroke-width: 0.2px;
+        -moz-osx-font-smoothing: grayscale;
+    }
 	.about_wrapper{
 		width: 100%;
 		padding: 5%;
         height: calc(100vh - 3.6rem);
 		background-color:@color-theme;
 		.userinfo{
-			height: 60vh;
 			// padding: 4rem;
             text-align: center;
 			border-radius: 10px;
@@ -68,7 +93,22 @@
             }
 			.base{
 				&_wrapper{
-
+                    padding-bottom: 20%;
+                    .item{
+                        text-align: left;
+                        padding:  0 10%;
+                        display: flex;
+                        align-items: center;
+                        line-height: 35px;
+                        i{
+                            font-size: 20px;
+                        }
+                        span{
+                            font-size: 1rem;
+                            display: inline-block;
+                            margin-left: 5%;
+                        }
+                    }
 				}
 				&_info{
 					height: 2rem;

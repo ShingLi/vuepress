5527a429cd2b171edebdf1d64b79709f895d7f8e
shingli <745573545@qq.com>
2018-08-30 22:22:09 +0800

    zzzz

--- docs/.vuepress/components/about.vue
+++ docs/.vuepress/components/about.vue
@@ -7,7 +7,10 @@
 					<el-aside width='20%'>
 						<div class="userinfo">
 				            <div class="avatar">
-				                <img :src="$withBase(`${avatar}?imageView2/1/w/80/h/80`)">
+				                <img :src="$withBase(`${avatar}?imageView2/1/w/80/h/80`)"
+                                    @mouseover='startRotate'
+                                    @mouseout= 'endRotate'
+                                >
 				            </div>
 							<div class="base_wrapper">
 								<div class="base_info">
@@ -24,33 +27,74 @@
 							</div>
 						</div>
 					</el-aside>
-					<el-main></el-main>
+					<el-main>
+                        <div class="main">
+                            <div class="detail">
+                                <h3>《卜算子 自嘲》</h3>
+                                <p>本是后山人， 偶做前堂客。 醉舞经阁半卷书， 坐井说天阔。</p>
+                                <p>大志戏功名， 海斗量福祸。 论到囊中羞涩时， 怒指乾坤错！</p>
+                            </div>
+                            <!-- 个人介绍 -->
+                            <lottie :options="defaultOptions"
+                                :height="200" 
+                                :width="200" 
+                                v-on:animCreated="handleAnimation" />
+                        </div>
+                    </el-main>
 				</el-container>
 			</el-col>
 		</el-row>
 	</div>
 </template>
 <script>
-	export default {
-		name:'about',
-		data() {
-			return {
-                name: '李成',
-                avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    import lottie from 'vue-lottie'
+    import * as animationData from "../public/json/loading.json";
+    export default {
+        name: "about",
+        data () {
+            return {
+                name: "李成",
+                avatar:
+                    "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
                 bossInfo: [
-                    {icon:'&#xe66e;',text:'男'},
-                    {icon:'&#xe626;',text:'汉'},
-                    {icon:'&#xe608;',text:'licheng'},
-                    {icon:'&#xe60d;',text:'安徽芜湖'},
-                    {icon:'&#xe670;',text:'745573545@qq.com'}
-
-                ]
-			}
-		},
-        created() {
-            this.iconClss = []
+                    { icon: "&#xe66e;", text: "男" },
+                    { icon: "&#xe626;", text: "汉" },
+                    { icon: "&#xe608;", text: "licheng" },
+                    { icon: "&#xe60d;", text: "安徽芜湖" },
+                    { icon: "&#xe670;", text: "745573545@qq.com" }
+                ],
+                defaultOptions: { animationData: animationData },
+                animationSpeed: 1,
+                anim: {}
+            };
+        },
+        created () {
+            this.iconClss = [];
+        },
+        methods: {
+            handleAnimation (anim) {
+                this.anim = anim;
+            },
+            startRotate (e) {
+                this.avatarRotate(e,true)
+            },
+            endRotate (e) {
+                this.avatarRotate(e,false)
+            },
+            avatarRotate (e, flag) {
+                const avatar = e.target
+                    avatar.style.transition = 'all 1s'
+                if (flag) {
+                    avatar.style.transform = `rotate(360deg)`
+                } else {
+                    avatar.style.transform = 'rotate(0)'
+                }
+            }
+        },
+        components: {
+            lottie
         }
-	}
+    };
 </script>
 <style lang="less" scoped>
     @import '../public/styles/variable.less';
@@ -130,6 +174,51 @@
 					}
 				}
 			}
-		}
-	}
+        }
+        .el-main{
+            margin-left: 5vw;
+            border-radius: 10px;
+            background: linear-gradient(#F56C6C 0,#EAE3D6 100%);
+            .detail {
+                padding-left: 2rem;
+                h3{
+                    text-indent: 3%;
+                    margin-bottom: 3%;
+                }
+                p{
+                    line-height: 35px;
+                    text-indent: 1%
+                }
+            }
+        }
+    }
+    html{
+        @media screen{
+            @media (min-width: 1024px){
+
+            } /*>=1024的设备*/
+
+            @media (min-width: 1100px) {
+
+            } /*>=1100的设备*/
+            @media (min-width: 1280px) {
+                
+            } /*>=1280的设备*/
+
+            @media (min-width: 1366px) {
+
+            }  
+
+            @media (min-width: 1440px) {
+                .el-aside{
+                    width: 17% !important;
+                }
+            } 
+            @media (min-width: 1680px) {
+            
+            } 
+            @media (min-width: 1920px) {
+            } 
+        }
+    }
 </style>

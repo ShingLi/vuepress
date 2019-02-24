c5a2b8a08b2a844fe9d7740cfb07f33630bfbb99
shingli <745573545@qq.com>
2019-02-24 22:12:15 +0800

    v-tap

--- docs/vue/自定义指令/vtap.js
+++ docs/vue/自定义指令/vtap.js
@@ -3,7 +3,7 @@
  * @Author: shingli
  * @LastEditors: Please set LastEditors
  * @Date: 2019-02-23 23:25:48
- * @LastEditTime: 2019-02-24 15:34:49
+ * @LastEditTime: 2019-02-24 22:04:59
  */
 
 const Tap = (function () {
@@ -11,25 +11,25 @@ const Tap = (function () {
         touchStart (e) {
             let touches = e.touches[0]
             // es6 编程风格推荐对象最后一个加逗号 对象静态化
-            const tapObj = {
+            const tapGather = {
                 timeStamp: + new Date (),
                 pageX: touches.pageX,
                 pageY: touches.pageY,
             }
-            Object.assign( this.tapObj, tapObj )
+            Object.assign( this.tapObj, tapGather )
         },
         touchEnd (e) {
             let touches = e.changedTouches[0]
-            const tapObj = {
+            const tapGather = {
                 timeStamp: +new Date() - this.tapObj.timeStamp,
                 distanceX: touches.pageX - this.tapObj.pageX,
                 distanceY: touches.pageY - this.tapObj.pageY,
             }
-            this.tapObj = { ...this.tapObj, ...tapObj }
+            this.tapObj = { ...this.tapObj, ...tapGather }
             // 判断是否是tap
             if (!handler.isTap(this)) return
             // 处理指令的事件
-            this.handler ()
+            this.handler (e)
         },
         isTap (slef) {
             return slef.tapObj.timeStamp < 300 && slef.tapObj.pageX < 10 && slef.tapObj.pageY < 10
@@ -38,9 +38,19 @@ const Tap = (function () {
     const tap = {
         inserted (el, binding) {
             el.tapObj = {}
-            el.handler = () => {
+            el.handler = e => {
                 let { value, modifiers } = binding
-                
+                if (modifiers.stop) e.stopPropagation()
+                if (modifiers.prevent)  e.preventDefault()
+
+                if (value && typeof value === 'object') {
+                    let { fn, args } = value
+                    args = args && Array.isArray(args) ? args.unshift(e) : [].unshift(e)
+                    if (typeof fn === 'function') {
+                        fn.call(this, args)
+                        args.shift(e)
+                    }
+                }
             }
             el.addEventListener ('touchstart', handler.touchStart)
             el.addEventListener ('touchend', handler.touchEnd)
@@ -52,6 +62,7 @@ const Tap = (function () {
             
             el.removeEventListener ('touchstart', handler.touchStart)
             el.removeEventListener ('touchend', handler.touchEnd)
+            el.handler = null
         },
         install (Vue) {
             Vue.directive('tap', tap)

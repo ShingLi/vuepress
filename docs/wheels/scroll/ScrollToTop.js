ba3a3d589fa19e22f351071da4275d61847ce77a
shingli <745573545@qq.com>
2019-03-17 23:00:04 +0800

    模仿造轮子

--- docs/wheels/scroll/ScrollToTop.js
+++ docs/wheels/scroll/ScrollToTop.js
@@ -1,7 +1,7 @@
 ;(function (window, options) {
     var utils = {
         extend (target) {
-            // shallow
+            // shallow jQuery 源码中一个方法
             var i = 1,k,
                 length = arguments.length;
             for (; i < length; i++) {
@@ -20,6 +20,32 @@
             } else {
                 element.style.filter = 'alpha(opacity=' + opacity + ')'
             }
+        },
+        addEvent (element, type, fn) {
+            var w = window
+            if (w.addEventListener) {
+                element.addEventListener (type, fn)
+            } else if (w.attachEvent) {
+                element.attachEvent('on'+type, fn)
+            }
+        },
+        scroll () {
+            if (window.pageXOffset) {
+                // 标准现在浏览器 ie9+
+                return {
+                    x: window.pageXOffset,
+                    y: window.pageYOffset
+                }
+            } else if (document.compatMode === 'CSS1Compat') {
+                return {
+                    x: document.documentElement.scrollLeft,
+                    y: document.documentElement.scrollTop
+                }
+            }
+            return {
+                x: document.body.scrollLeft,
+                y: document.body.scrollTop
+            }
         }
     }
     function ScrollToTop (element, options) {
@@ -34,10 +60,19 @@
     var proto = ScrollToTop.prototype
     proto.init = function () {
         this.hideElement ()
+        this.bindScrollEvent ()
     }
     proto.hideElement = function () {
         utils.setOpacity (this.element, 0)
         this.status = 'hide'
+    }
+    proto.bindScrollEvent = function () {
+        var slef = this
+        utils.addEvent (window, 'scroll', function () {
+            if (utils.scroll().y > slef.options.showWhen) {
+                
+            }
+        })
     }
     if (!window.ScrollToTop) window.ScrollToTop = ScrollToTop 
 }(window))

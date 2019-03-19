775e7749c380e6edebb1fd563a48c8ee7d104c62
shingli <745573545@qq.com>
2019-03-19 22:47:10 +0800

    class重写

--- docs/wheels/scroll/ScrollToTop.js
+++ docs/wheels/scroll/ScrollToTop.js
@@ -1,78 +1,75 @@
-;(function (window, options) {
-    var utils = {
-        extend (target) {
-            // shallow jQuery 源码中一个方法
-            var i = 1,k,
-                length = arguments.length;
-            for (; i < length; i++) {
-                var currentVal = arguments[i]
-                for (k in currentVal) {
-                    if (currentVal[k] && currentVal.hasOwnProperty(k)) {
-                        target[k] = currentVal[k]
-                    }
-                }
-            }
-            return target
-        },
-        setOpacity (element, opacity) {
+;(function () {
+    let defaultOptions = Symbol()
+    const utils = {
+        setOpacity (element = this.element, opacity = 0) {
             if (element.style.opacity != null) {
                 element.style.opacity = opacity / 100
             } else {
-                element.style.filter = 'alpha(opacity=' + opacity + ')'
+                element.style.filter = `alpha(opacity=${opacity})`
             }
         },
-        addEvent (element, type, fn) {
-            var w = window
+        addEvent (type, fn) {
+            const w = window
             if (w.addEventListener) {
-                element.addEventListener (type, fn)
-            } else if (w.attachEvent) {
-                element.attachEvent('on'+type, fn)
+                w.addEventListener(type, fn)
+            } else {
+                w.attachEvent (`on${type}`, fn)
             }
         },
         scroll () {
             if (window.pageXOffset) {
-                // 标准现在浏览器 ie9+
                 return {
                     x: window.pageXOffset,
-                    y: window.pageYOffset
+                    y: window.pageYOffset,
                 }
-            } else if (document.compatMode === 'CSS1Compat') {
+            } else if (document.compatMode == 'CSS1Compat') {
                 return {
                     x: document.documentElement.scrollLeft,
                     y: document.documentElement.scrollTop
                 }
+            } else {
+                return {
+                    x: document.body.scrollLeft,
+                    y: document.body.scrollTop
+                }
             }
-            return {
-                x: document.body.scrollLeft,
-                y: document.body.scrollTop
-            }
+        },
+        fadeIn () {
+            
         }
     }
-    function ScrollToTop (element, options) {
-        this.element = typeof element === 'string' ? document.querySelector(element) : element
-        this.options = utils.extend( {}, this.constructor.defaultOptions, options )
-        this.init ()
+    class ScrollToTop {
+        constructor (element, options) {
+            this.element  = typeof element == 'string' ? document.querySelector(element) : element
+            this.options = { ...this[defaultOptions], ...options }
+            this.init ()
+        }
+        init () {
+            this.hideElment ()
+            this.bindScrollEvent ()
+        }
+        hideElment () {
+            utils.setOpacity.apply(this)
+            this.status = 'hide'
+        }
+        bindScrollEvent () {
+            utils.addEvent ('scroll', () => {
+                if (utils.scroll().y > this.options.showWhen) {
+                    if (this.status == 'hide') {
+                        utils.fadeIn ()
+                        this.status = 'show'
+                    }
+                } else {
+                    if (this.status == 'show') {
+                        utils.fadeOut ()
+                        this.status = 'hide'
+                    }
+                }
+            })
+        }
+        [defaultOptions] = {
+            showWhen: 300
+        }
     }
-    ScrollToTop.defaultOptions = {
-        showWhen: 300,
-        fadeSpeed: 10
-    }
-    var proto = ScrollToTop.prototype
-    proto.init = function () {
-        this.hideElement ()
-        this.bindScrollEvent ()
-    }
-    proto.hideElement = function () {
-        utils.setOpacity (this.element, 0)
-        this.status = 'hide'
-    }
-    proto.bindScrollEvent = function () {
-        var slef = this
-        utils.addEvent (window, 'scroll', function () {
-            if (utils.scroll().y > slef.options.showWhen) {
-                
-            }
-        })
-    }
-    if (!window.ScrollToTop) window.ScrollToTop = ScrollToTop 
-}(window))
+    if (!self.ScrollToTop) self.ScrollToTop = ScrollToTop
+}())

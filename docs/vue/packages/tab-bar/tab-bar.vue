8821935507b81350a7e552edaf7ba780590c1f05
shingli <745573545@qq.com>
2019-05-21 23:44:46 +0800

    tab-bar

---
+++ docs/vue/packages/tab-bar/tab-bar.vue
@@ -0,0 +1,106 @@
+<script>
+    import TabItem from './tab-item'
+    export default {
+        name: 'tab-bar',
+        props: {
+            data: {
+                type: Array,
+                default: () => []
+            },
+            value: {
+                type: [String, Number],
+                required: true
+            },
+            transition: {
+                type: Boolean,
+                default: true
+            }
+        },
+        components: {
+            TabItem
+        },
+        watch: {
+            value (newVal) {
+                this._updateSliderStyle()
+            }
+        },
+        created () {
+            this.tabs = []
+        },
+        mounted () {
+            this._updateSliderStyle ()
+        },
+        methods: {
+            trigger (val) {
+                this.$emit('input', val)
+            },
+            addTabs (tab) {
+                this.tabs.push(tab)
+            },
+            _updateSliderStyle () {
+                let { width, index } = this._calculateWidthAndIndex()
+
+                const slider = this.$refs.slider
+                let offset = this._calculateOffsetLeft(index) + 'px'
+
+                this.$nextTick(() => {
+                    slider.style.width = `${width}px`
+                    if (this.transition) {
+                        slider.style.transition = 'transform .3s linear'
+                    }
+                    slider.style.transform = `translateX(${offset})`
+                })
+            },
+            _calculateWidthAndIndex () {
+                let width = 0, index = 0;
+                if (this.tabs.length) {
+                    index = this.tabs.findIndex(item => { return item.label == this.value })
+                    width = this.tabs[index].$el.clientWidth
+                }
+                return {
+                    width,
+                    index
+                }
+            },
+            _calculateOffsetLeft (index) {
+                return this.tabs[index].$el.offsetLeft
+            }
+        },
+        render () {
+            return (
+                <div class="tab-bar">
+                    <slot>
+                        {
+                            this.data.length &&
+                            this.data.map((item, index) => {
+                                return (
+                                    <tab-item
+                                        label = {item.label}
+                                        icon = {item.icon}
+                                    ></tab-item>
+                                )
+                            })
+                        }
+                    </slot>
+                    <div class="slider" ref= 'slider'></div>
+                </div>
+            )
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .tab-bar{
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .slider {
+            position: absolute;
+            height: 2px;
+            bottom: 0;
+            width: 20px;
+            left: 0;
+            background-color: #f01414;
+        }
+    }
+</style>
---
+++ docs/vue/packages/tab-bar/tab-item.vue
@@ -0,0 +1,65 @@
+<!--
+ * @Description: tab-item
+ * @Author: shingli
+ * @Date: 2019-05-19 15:12:17
+ * @LastEditTime: 2019-05-21 22:58:39
+ * @LastEditors: Please set LastEditors
+ -->
+
+<script>
+    const tab = {
+        name: 'tab-item',
+        props: {
+            label: {
+                type: [String, Number],
+                required: true
+            },
+            icon:{
+                type: String,
+                default: ''
+            },
+        },
+        computed: {
+            isActive () {
+                return this.$parent.value == this.label
+            }
+        },
+        created () {
+            this.$parent.addTabs(this)
+        },
+        methods:{
+            handleClick () {
+                this.$parent.trigger(this.label)
+            }
+        },
+        render () {
+            // console.log(this)
+            let className = 'tab-item'
+            className += this.isActive ? ' tab-active-color' : ''
+            return (
+                <div class= { className }
+                    onClick = { this.handleClick }
+                >
+                    <slot name='icon'>
+                        <i class= {this.icon} ></i>
+                    </slot>
+                    <slot>
+                        <div domPropsInnerHTML = {this.label}></div>
+                    </slot>
+                </div>
+            )
+        }
+    }
+    export default tab
+</script>
+<style lang="scss" scoped>
+    .tab-item{
+        flex:1;
+        padding: 13px 0;
+        color: #666;
+        text-align: center;
+    }
+    .tab-active-color{
+        color: #f01414;
+    }
+</style>

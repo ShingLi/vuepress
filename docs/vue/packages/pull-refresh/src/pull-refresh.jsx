05d3f14cf7a32b8a930a2d31a2da8123d1d5929a
shingli <745573545@qq.com>
2019-06-04 00:19:31 +0800

    JSX

--- docs/vue/packages/pull-refresh/src/pull-refresh.jsx
+++ docs/vue/packages/pull-refresh/src/pull-refresh.jsx
@@ -4,7 +4,11 @@ const pullRefresh = {
 	name: 'pull-refresh',
 	data () {
 		return {
-			supportPassive: false
+			supportPassive: false,
+			mv: 0,
+			threshould: 60,
+			maxOffset: 80,
+			status: 'pending'
 		}
 	},
 	created () {
@@ -15,14 +19,45 @@ const pullRefresh = {
 			window.addEventListener('test', null , options)
 		}catch (e) {console.log(e)}
 
+		this.init ()
 	},
 	mounted () {
 		setTimeout(() => {
-			
 			this.dom = document.querySelector('.refresh-wrapper')
 		},20)
 	},
 	methods: {
+		init () {
+			this.bindEvent(window, 'touchstart', this.touchStart)
+			this.bindEvent(window, 'touchmove', this.touchMove)
+			this.bindEvent(window, 'touchend', this.touchEnd)
+		},
+		bindEvent (el, type, f) {
+			if (addEventListener) {
+				el.addEventListener(type, f, this.supportPassive ? { passive:false }: false)
+			}
+		},
+		touchStart (e) {
+			if (this.shouldRefresh()) {
+				this.pullStartY = e.touches[0].screenY
+			}
+		},
+		touchMove (e) {
+			let pullMoveY = e.touches[0].screenY, dist
+			if (this.pullStartY && pullMoveY) {
+				dist = pullMoveY - this.pullStartY
+			}
+			if (dist && dist > 0) {
+				e.cancelable && e.preventDefault()
+				this.dom.style.minHeight = this.mv + 'px'
+			}
+		},
+		touchEnd (e) {
+
+		},
+		shouldRefresh () {
+			return !scrollY
+		}
 
 	},
 	render () {

e11813d5144d0a1f72812ee39c218415d580504d
shingli <745573545@qq.com>
2019-06-09 22:18:23 +0800

    tab 切换

---
+++ docs/vue/packages/tab/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: tab 切换组件
+ * @Author: shingli
+ * @Date: 2019-06-09 22:00:16
+ * @LastEditTime: 2019-06-09 22:15:27
+ * @LastEditors: Please set LastEditors
+ */
+
---
+++ docs/vue/packages/tab/src/tab.jsx
@@ -0,0 +1,9 @@
+// 模仿一个有赞的tab切换组件，用jsx 的形式
+
+const Tab = {
+	name: 'lc-tab',
+	render () {
+		return (
+		)
+	}
+}
